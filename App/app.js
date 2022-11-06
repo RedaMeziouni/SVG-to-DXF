0f24141c6d4fd967a0aae5cbd044efd9bff7aefe
RedaMeziouni <meziounidb@gmail.com>
2022-11-07 00:47:33 +0100

    switch to jquery and finish the first version

--- App/app.js
+++ App/app.js
@@ -1,44 +1,116 @@
-let canvas = new fabric.Canvas("canvas");
-let clear = document.getElementById("clear");
-let rectangle = document.getElementById("rectangle");
-let ellipse = document.getElementById("ellipse");
-let convert = document.getElementById("convert");
-let success = document.getElementById("success");
-let isEllipse = false;
-let isRectangle = false;
-
-let circle, isDown, origX, origY;
-
-// rectangle
-document.querySelector(rectangle).addEventListener("click", () => {
-isEllipse = false;
-isRectangle = true;
-console.log("rectangle");
+var canvas = new fabric.Canvas("canvas");
+var clear = document.getElementById("clear");
+var rectangle = document.getElementById("rectangle");
+var ellipse = document.getElementById("ellipse");
+var convert = document.getElementById("convert");
+var success = document.getElementById("success");
+var isEllipse = false;
+var isRectangle = false;
+ 
+var circle, isDown, origX, origY;
+ 
+$(rectangle).on("click", function () {
+ isEllipse = false;
+ isRectangle = true;
+ 
+ console.log("rectangle");
 });
-
-// ellipse
-document.querySelector(ellipse).addEventListener("click", () => {
-    isEllipse = true;
-    isRectangle = false;
-    console.log("ellipse");
+ 
+$(ellipse).on("click", function () {
+ isEllipse = true;
+ isRectangle = false;
+ console.log("ellipse");
 });
-
-// Clear
-document.querySelector(clear).addEventListener("click", () => {
-    let objects = canvas.getObjects();
-    for (let i = 0; i < objects.length; i++) {
-      canvas.remove(objects[i]);
-    }
-    canvas.renderAll();
+$(clear).on("click", function () {
+ var objects = canvas.getObjects();
+ for (var i = 0; i < objects.length; i++) {
+   canvas.remove(objects[i]);
+ }
+ canvas.renderAll();
 });
-
-// Convert
-document.querySelector(convert).addEventListener("click", async function () {
-    let mySvg = canvas.toSVG();
-    let response = await fetch("http://127.0.0.1:3000/", {
-      method: "POST",
-      body: mySvg,
-    }).then(function (response) {
-      alert("SVG Converted");
-    });
+ 
+$(convert).on("click", async function () {
+ let mySvg = canvas.toSVG();
+ console.log(mySvg);
+ let response = await fetch("http://127.0.0.1:3000/", {
+   method: "POST",
+   body: mySvg,
+ }).then(function (response) {
+   alert("Order Placed");
+ });
+ 
+});
+canvas.on("mouse:down", function (o) {
+ isDown = true;
+ var pointer = canvas.getPointer(o.e);
+ origX = pointer.x;
+ origY = pointer.y;
+ 
+ if (isEllipse) {
+   circle = new fabric.Circle({
+     left: origX,
+     top: origY,
+     originX: "left",
+     originY: "top",
+     radius: pointer.x - origX,
+     angle: 0,
+     fill: "",
+     stroke: "red",
+     strokeWidth: 3,
+   });
+   canvas.add(circle);
+ }
+ 
+ if (isRectangle) {
+   rectangle = new fabric.Rect({
+     left: origX,
+     top: origY,
+     fill: "transparent",
+     stroke: "red",
+     strokeWidth: 3,
+   });
+   canvas.add(rectangle);
+ }
+});
+ 
+canvas.on("mouse:move", function (o) {
+ if (!isDown) return;
+ var pointer = canvas.getPointer(o.e);
+ var radius =
+   Math.max(Math.abs(origY - pointer.y), Math.abs(origX - pointer.x)) / 2;
+ if (isEllipse) {
+   if (radius > circle.strokeWidth) {
+     radius -= circle.strokeWidth / 2;
+   }
+   circle.set({ radius: radius });
+ 
+   if (origX > pointer.x) {
+     circle.set({ originX: "right" });
+   } else {
+     circle.set({ originX: "left" });
+   }
+   if (origY > pointer.y) {
+     circle.set({ originY: "bottom" });
+   } else {
+     circle.set({ originY: "top" });
+   }
+   canvas.renderAll();
+ }
+ 
+ if (isRectangle) {
+   if (origX > pointer.x) {
+     rectangle.set({ left: Math.abs(pointer.x) });
+   }
+   if (origY > pointer.y) {
+     rectangle.set({ top: Math.abs(pointer.y) });
+   }
+ 
+   rectangle.set({ width: Math.abs(origX - pointer.x) });
+   rectangle.set({ height: Math.abs(origY - pointer.y) });
+   canvas.renderAll();
+ }
+});
+ 
+canvas.on("mouse:up", function (o) {
+ isDown = false;
 });

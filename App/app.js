ec53949ec4faf0261d1f3e291abe543c14753a74
RedaMeziouni <meziounidb@gmail.com>
2022-11-06 23:30:31 +0100

    grab necessar yelements from the DOM

---
+++ App/app.js
@@ -0,0 +1,8 @@
+let canvas = new fabric.Canvas("canvas");
+let clear = document.getElementById("clear");
+let rectangle = document.getElementById("rectangle");
+let ellipse = document.getElementById("ellipse");
+let convert = document.getElementById("convert");
+let success = document.getElementById("success");
+let isEllipse = false;
+let isRectangle = false;
